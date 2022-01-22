4c4d9df168e92c269df73afa1089d2b8522cc3d0
welbber <welbervital01@gmail.com>
2022-01-22 13:08:54 -0300

    Alteração forma de filtrar pelos parametros, alteração pemissões admin

--- src/main/java/com/github/aniversario/config/SecurityConfig.java
+++ src/main/java/com/github/aniversario/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.github.aniversario.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,23 +11,46 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private Environment env;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.inMemoryAuthentication().withUser("admin").password("^%y&RQCzm44F").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+        http.cors().and().csrf().disable();
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/github/aniversario/service/BirthdayPersonService.java
+++ src/main/java/com/github/aniversario/service/BirthdayPersonService.java
@@ -5,8 +5,6 @@ import com.github.aniversario.entities.BirthdayPerson;
 import com.github.aniversario.repositories.BirthdayPersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,13 +20,13 @@ public class BirthdayPersonService {
     }
 
     public List<BirthdayPersonDTO> birthdayOfTheDay(Integer day, Integer month) {
-        List<BirthdayPerson> result = birthdayPersonRepository.findAll();
-        return result.stream().map(x -> new BirthdayPersonDTO(x)).collect(Collectors.toList()).stream().filter(x -> x.getDay() == day && x.getMonth() == month).collect(Collectors.toList());
+        List<BirthdayPerson> result = birthdayPersonRepository.findByDayAndMonth(day, month);
+        return result.stream().map(x -> new BirthdayPersonDTO(x)).collect(Collectors.toList());
     }
 
     public List<BirthdayPersonDTO> birthOfTheMonth(Integer month) {
-        List<BirthdayPerson> result = birthdayPersonRepository.findAll();
-        return result.stream().map(x -> new BirthdayPersonDTO(x)).collect(Collectors.toList()).stream().filter(x -> x.getMonth() == month).collect(Collectors.toList());
+        List<BirthdayPerson> result = birthdayPersonRepository.findByMonth(month);
+        return result.stream().map(x -> new BirthdayPersonDTO(x)).collect(Collectors.toList());
     }
 
     public BirthdayPerson save(BirthdayPerson birthdayPerson) {
